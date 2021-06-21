f3dd42bac8542c8a595fe3a44b9b98b861f0456c
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-06-21 23:17:12 +0300

    add config.enabled=false as bootstrap property

--- src/test/java/com/vpnbeast/vpnbeastservice/VpnbeastServiceApplicationTests.java
+++ src/test/java/com/vpnbeast/vpnbeastservice/VpnbeastServiceApplicationTests.java
@@ -3,8 +3,7 @@ package com.vpnbeast.vpnbeastservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @ActiveProfiles("unit-test")
-@SpringBootTest(properties = {"spring.cloud.config.enabled=false"})
+@SpringBootTest
 public class VpnbeastServiceApplicationTests {
 
     @Test
